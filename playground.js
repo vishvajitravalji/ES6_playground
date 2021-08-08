444f3941a0a2c948dc5f54983233210476a90de9
Vish <vishvajitravalji@gmail.com>
2021-08-08 17:38:11 -0400

    exp

--- playground.js
+++ playground.js
@@ -1,17 +1,11 @@
 var products = [
-    { name:'tomatos' , type:'vegetable' , quantity: 0 , price: 1 },
-    { name:'banana' , type:'fruit' , quantity: 10 , price: 15  },
-    { name:'garlic' , type:'vegetable' , quantity: 30 , price: 10  },
-    { name:'apple' , type:'fruit' , quantity: 3 , price: 5  },
-    { name:'beans' , type:'vegetable' , quantity: 35 , price: 9  },
+    { name:'tomatos' , type:'vegetable' },
+    { name:'banana' , type:'fruit' },
+    { name:'garlic' , type:'vegetable' },
+    { name:'apple' , type:'fruit' },
+    { name:'beans' , type:'vegetable' },
 ];
 
-var filteredProducts = [] ; 
+var filtededValue = [];
 
-filteredProducts = products.filter(product => {
-    return product.type ==='fruit' && product.quantity > 0 && product.price <= 10
-});
-
-console.log(filteredProducts);
-
-
+fil
